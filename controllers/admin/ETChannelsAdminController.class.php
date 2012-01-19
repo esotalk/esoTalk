4367994055e5b5911418a13e7c42a02211fdf79e
Toby Zerner <toby@esotalk.org>
2012-01-19 18:30:04 +1030

    Fix #21 - guests no longer see channels which are unsubscribed by default

--- controllers/admin/ETChannelsAdminController.class.php
+++ controllers/admin/ETChannelsAdminController.class.php
@@ -49,6 +49,7 @@ public function edit($channelId = "")
  	$form = ETFactory::make("form");
  	$form->action = URL("admin/channels/edit/".$channel["channelId"]);
  	$form->setValues($channel);
+ 	$form->setValues($channel["attributes"]);
 
 	// Get a list of groups!
  	$groups = ET::groupModel()->getAll();
