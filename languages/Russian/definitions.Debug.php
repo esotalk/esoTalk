90150f2cbed40303d0eb75857889d63fbe843446
Viktor Matushevskyi <viktorminator@gmail.com>
2013-01-29 14:07:55 +0200

    Russian lang package added
    All credits go to ext2 user http://esotalk.org/forum/member/656-ext2
    Все спасибо за проделанную работу идут к пользователю ext2 с форума.

---
+++ languages/Russian/definitions.Debug.php
@@ -0,0 +1,10 @@
+<?php
+// Copyright 2011 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+// Russian Definitions for the Debug plugin.
+
+$definitions["MySQL queries"] = "MySQL запросы";
+$definitions["Page loaded in %s seconds"] = "Страница загружена за %s секунд";
+$definitions["POST + GET + FILES information"] = "POST + GET + FILES информация";
+$definitions["SESSION + COOKIE information"] = "SESSION + COOKIE информация";
