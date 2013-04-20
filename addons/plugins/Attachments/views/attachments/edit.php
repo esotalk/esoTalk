d59cc3c08e3f299d8b8ccebb84f73d45536eb90f
Toby Zerner <toby.zerner@gmail.com>
2013-04-20 17:17:18 +0930

    add security checks

--- addons/plugins/Attachments/views/attachments/edit.php
+++ addons/plugins/Attachments/views/attachments/edit.php
@@ -1,3 +1,10 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+if (!defined("IN_ESOTALK")) exit;
+
+?>
 <div class='attachments-edit'>
 	<ul>
 		<?php foreach ($data["attachments"] as $attachment): ?>
--- addons/plugins/Attachments/views/attachments/list.php
+++ addons/plugins/Attachments/views/attachments/list.php
@@ -1,3 +1,10 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+if (!defined("IN_ESOTALK")) exit;
+
+?>
 <div class='attachments'>
 	<h4><span>Attachments</span></h4>
 	<ul>
