a2d1facd48078d3b7d0d0e733283efedadb5878c
Toby Zerner <toby.zerner@gmail.com>
2013-04-20 11:15:29 +0930

    oops! left some debug code in there

--- skins/Default/skin.php
+++ skins/Default/skin.php
@@ -36,7 +36,7 @@ public function handler_init($sender)
 	$sender->addCSSFile($this->getResource("styles.css"), true);
 
 	// If we're viewing from a mobile browser, add the mobile CSS and change the master view.
-	if (!$isMobile = isMobileBrowser()) {
+	if ($isMobile = isMobileBrowser()) {
 		$sender->addCSSFile($this->getResource("mobile.css"), true);
 		$sender->masterView = "mobile.master";
 		$sender->addToHead("<meta name='viewport' content='width=device-width; initial-scale=1.0; maximum-scale=1.0;'>");
