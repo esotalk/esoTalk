7d216f5d7a384dbf190c2c832527c9a6d932cd88
Toby Zerner <toby.zerner@gmail.com>
2013-04-21 09:35:56 +0930

    remove some emoticons that often are converted accidentally: XP, XD, B)

--- addons/plugins/Emoticons/plugin.php
+++ addons/plugins/Emoticons/plugin.php
@@ -85,10 +85,10 @@ public function handler_format_format($sender)
 	$styles[">:["] = "background-position:0 -480px";
 	$styles["._."] = "background-position:0 -500px";
 	$styles["T_T"] = "background-position:0 -520px";
-	$styles["XD"] = "background-position:0 -540px";
+	// $styles["XD"] = "background-position:0 -540px";
 	$styles["('<"] = "background-position:0 -560px";
-	$styles["B)"] = "background-position:0 -580px";
-	$styles["XP"] = "background-position:0 -600px";
+	// $styles["B)"] = "background-position:0 -580px";
+	// $styles["XP"] = "background-position:0 -600px";
 	$styles[":S"] = "background-position:0 -620px";
 	$styles["=S"] = "background-position:0 -620px";
 	$styles[">:)"] = "background-position:0 -640px";
