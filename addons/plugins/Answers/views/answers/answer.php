61446e58d672336efbd5e1ec30093c87ed3c2963
Toby Zerner <toby.zerner@gmail.com>
2013-07-23 13:22:21 +0930

    answers plugin

---
+++ addons/plugins/Answers/views/answers/answer.php
@@ -0,0 +1,29 @@
+<?php
+// Copyright 2013 Toby Zerner, Simon Zerner
+// This file is part of esoTalk. Please see the included license file for usage information.
+
+if (!defined("IN_ESOTALK")) exit;
+
+$conversation = $data["conversation"];
+$post = $data["answer"];
+?>
+<div class='answer thing hasControls'>
+	<div class='postHeader'>
+		<div class='info'>
+			<h3><i class="icon-ok-sign"></i> Answered by <?php echo memberLink($post["memberId"], $post["username"]); ?></h3>
+			<a href='<?php echo URL(postURL($post["postId"])); ?>' rel='post' data-id='<?php echo $post["postId"]; ?>'>See post in context</a>
+		</div>
+		<div class='controls'>
+			<?php if ($conversation["startMemberId"] == ET::$session->userId): ?>
+			<a href='<?php echo URL("conversation/unanswer/".$conversation["conversationId"]."?token=".ET::$session->token); ?>' title='<?php echo T("Remove answer"); ?>' class='control-unanswer'><i class='icon-remove'></i></a>
+			<?php endif; ?>
+		</div>
+	</div>
+
+	<div class='postBody'>
+		<?php
+		$words = ET::$session->get("highlight");
+		echo ET::formatter()->init($post["content"])->highlight($words)->format()->get();
+		?>
+	</div>
+</div>
