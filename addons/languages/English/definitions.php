68c9709bface7ba3f26eb86ca1fd12753036c2b2
Toby Zerner <toby.zerner@gmail.com>
2014-01-13 08:12:34 +1030

    add definition "mark as unread". closes #131

--- addons/languages/English/definitions.php
+++ addons/languages/English/definitions.php
@@ -228,6 +228,7 @@ $definitions["Manage Groups"] = "Manage Groups";
 $definitions["Manage Languages"] = "Manage Languages";
 $definitions["Manage your forum's channels (categories)"] = "Manage your forum's channels (categories)";
 $definitions["Mark as read"] = "Mark as read";
+$definitions["Mark as unread"] = "Mark as unread";
 $definitions["Mark all as read"] = "Mark all as read";
 $definitions["Mark listed as read"] = "Mark listed as read";
 $definitions["Maximum size of %s. %s."] = "Maximum size of %s. %s.";
