36512c42d29e5ccadd7ac837249790ba944e0895
Toby Zerner <toby.zerner@gmail.com>
2014-04-14 22:48:42 +0930

    default skin overhaul!

--- addons/skins/Default/skin.php
+++ addons/skins/Default/skin.php
@@ -43,45 +43,36 @@ public function handler_init($sender)
 		$sender->addToHead("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>");
 	}
 
-	// If custom colors have been set in this skin's settings, add some CSS to the page.
-	$styles = array();
+	$sender->addCSSFile("config/colors.css", true);
 
-	// If a custom header color has been set...
-	if ($c = C("skin.Default.headerColor")) {
-		$styles[] = "#hdr {background-color:$c}";
+	if (!C("skin.Default.primaryColor")) $this->writeColors("#364159");
+}
 
-		// If the header color is in the top half of the lightness spectrum, add the "lightHdr" class to the body.
-		$rgb = colorUnpack($c, true);
-		$hsl = rgb2hsl($rgb);
-		if ($hsl[2] >= 0.5) $sender->bodyClass .= " lightHdr";
-	}
 
-	// If a custom body color has been set...
-	if ($c = C("skin.Default.bodyColor")) {
-		$styles[] = "body, .scrubberMore {background-color:$c !important}";
+/**
+ * Write the skin's color configuration and CSS.
+ * 
+ * @param string $primary The primary color.
+ * @return void
+ */
+protected function writeColors($primary)
+{
+	ET::writeConfig(array("skin.Default.primaryColor" => $primary));
 
-		// If the body color is in the bottom half of the lightness spectrum, add the "darkBody" class to the body.
-		$rgb = colorUnpack($c, true);
-		$hsl = rgb2hsl($rgb);
-		if ($hsl[2] < 0.5) $sender->bodyClass .= " darkBody";
+	$rgb = colorUnpack($primary, true);
+	$hsl = rgb2hsl($rgb);
+	$hsl[1] = min(0.4, $hsl[1]);
+	$hsl[2] = min(0.4, $hsl[2]);
 
-		// Slightly darken the body color and set it as the border color for the body content area.
-		$hsl[2] = max(0, $hsl[2] - 0.1);
-		$hsl[1] = min($hsl[1], 0.5);
-		$b = colorPack(hsl2rgb($hsl), true);
-		$styles[] = "#body-content {border-color:$b}";
-	}
+	$primary = colorPack(hsl2rgb($hsl), true);
 
-	// If a custom body background image has been set...
-	if (($img = C("skin.Default.bodyImage")) and !$isMobile)
-		$styles[] = "body {background-image:url(".getWebPath($img)."); background-position:top center; background-attachment:fixed}";
-	
-	// Do we want this background image to not repeat?
-	if ($img and C("skin.Default.noRepeat"))
-		$styles[] = "body {background-repeat:no-repeat}";
+	$hsl[1] = 0;
+	$secondary = colorPack(hsl2rgb(array(2 => 0.6) + $hsl), true);
+	$tertiary = colorPack(hsl2rgb(array(2 => 0.9) + $hsl), true);
 
-	// If we have any custom styles at all, add them to the page head.
-	if (count($styles)) $sender->addToHead("<style type='text/css'>\n".implode("\n", $styles)."\n</style>");
+	$css = file_get_contents($this->getResource("colors.css"));
+	$css = str_replace(array("{primary}", "{secondary}", "{tertiary}"), array($primary, $secondary, $tertiary), $css);
+	file_put_contents(PATH_CONFIG."/colors.css", $css);
 }
 
 
@@ -97,33 +88,14 @@ public function settings($sender)
 	// Set up the settings form.
 	$form = ETFactory::make("form");
 	$form->action = URL("admin/appearance");
-	$form->setValue("headerColor", C("skin.Default.headerColor"));
-	$form->setValue("bodyColor", C("skin.Default.bodyColor"));
-	$form->setValue("noRepeat", (bool)C("skin.Default.noRepeat"));
-	$form->setValue("bodyImage", (bool)C("skin.Default.bodyImage"));
+	$form->setValue("primaryColor", C("skin.Default.primaryColor"));
 
 	// If the form was submitted...
 	if ($form->validPostBack("save")) {
+		$this->writeColors($form->getValue("primaryColor"));
 
-		// Construct an array of config options to write.
-		$config = array();
-		$config["skin.Default.headerColor"] = $form->getValue("headerColor");
-		$config["skin.Default.bodyColor"] = $form->getValue("bodyColor");
-
-		// Upload a body bg image if necessary.
-		if ($form->getValue("bodyImage") and !empty($_FILES["bodyImageFile"]["tmp_name"])) $config["skin.Default.bodyImage"] = $this->uploadBackgroundImage($form);
-		elseif (!$form->getValue("bodyImage")) $config["skin.Default.bodyImage"] = false;
-		$config["skin.Default.noRepeat"] = (bool)$form->getValue("noRepeat");
-
-		if (!$form->errorCount()) {
-
-			// Write the config file.
-			ET::writeConfig($config);
-
-			$sender->message(T("message.changesSaved"), "success autoDismiss");
-			$sender->redirect(URL("admin/appearance"));
-
-		}
+		$sender->message(T("message.changesSaved"), "success autoDismiss");
+		$sender->redirect(URL("admin/appearance"));
 	}
 
 	$sender->data("skinSettingsForm", $form);
@@ -133,37 +105,4 @@ public function settings($sender)
 }
 
 
-/**
- * Upload a background image.
- * 
- * @return void
- */
-protected function uploadBackgroundImage($form)
-{
-	$uploader = ET::uploader();
-
-	try {
-
-		// Validate and get the uploaded file from this field.
-		$file = $uploader->getUploadedFile("bodyImageFile");
-
-		// Save it as an image, restricting it to a maximum size.
-		$bg = $uploader->saveAsImage($file, PATH_UPLOADS."/bg", 1, 1, "min");
-		$bg = str_replace(PATH_UPLOADS, "uploads", $bg);
-
-		// Delete the old background image (if we didn't just overwrite it.)
-		if ($bg != C("skin.Default.bodyImage")) @unlink(C("skin.Default.bodyImage"));
-
-		return $bg;
-
-	} catch (Exception $e) {
-
-		// If something went wrong up there, add the error message to the form.
-		$form->error("bodyImageFile", $e->getMessage());
-
-	}
 }
-
-}
-
-?>
--- addons/skins/Default/views/settings.php
+++ addons/skins/Default/views/settings.php
@@ -19,28 +19,9 @@ $form = $data["skinSettingsForm"];
 
 <li class='sep'></li>
 
-<li id='headerColor'>
-<label><?php echo T("Header color"); ?></label>
-<?php echo $form->input("headerColor", "text", array("class" => "color")); ?> <a href='#' class='reset'><?php echo T("Reset"); ?></a>
-</li>
-
-
-<li id='bodyColor'>
-<label><?php echo T("Background color"); ?></label>
-<?php echo $form->input("bodyColor", "text", array("class" => "color")); ?> <a href='#' class='reset'><?php echo T("Reset"); ?></a>
-</li>
-
-<li class='sep'></li>
-
-<li id='bodyImage'>
-<label><?php echo T("Background image"); ?></label>
-<div class='checkboxGroup'>
-<label class='checkbox'><?php echo $form->checkbox("bodyImage"); ?> <?php echo T("Use a background image"); ?></label>
-<div class='indent'>
-<?php echo $form->input("bodyImageFile", "file", array("class" => "text")); ?>
-<label class='checkbox'><?php echo $form->checkbox("noRepeat"); ?> <?php echo T("Don't repeat"); ?></label>
-</div>
-</div>
+<li id='primaryColor'>
+<label><?php echo T("Primary color"); ?></label>
+<?php echo $form->input("primaryColor", "text", array("class" => "color")); ?> <a href='#' class='reset'><?php echo T("Reset"); ?></a>
 </li>
 
 <li class='sep'></li>
@@ -53,97 +34,8 @@ $form = $data["skinSettingsForm"];
 <script>
 $(function() {
 
-	// Turn a normal text input into a color picker, and run a callback when the color is changed.
-	function colorPicker(id, callback) {
-
-		// Create the color picker container.
-		var picker = $("<div id='"+id+"-colorPicker'></div>").appendTo("body").addClass("popup").hide();
-
-		// When the input is focussed upon, show the color picker.
-		$("#"+id+" input").focus(function() {
-			picker.css({position: "absolute", top: $(this).offset().top - picker.outerHeight(), left: $(this).offset().left}).show();
-		})
-
-		// When focus is lost, hide the color picker.
-		.blur(function() {
-			picker.hide();
-		})
-
-		// When a value is typed in the field, update the color.
-		.keyup(function() {
-			farb.setColor($(this).val());
-		})
-
-		// Add a color swatch before the input.
-		.before("<span class='colorSwatch'></span>");
-
-		// Create a handler function for when the color is changed to update the input and swatch, and call
-		// the custom callback function.
-		var handler = function(color) {
-			callback(color, picker);
-			$("#"+id+" input").val(color.toUpperCase());
-			$("#"+id+" .colorSwatch").css("backgroundColor", color);
-			$("#"+id+" .reset").toggle(!!color);
-		}
-
-		// Set up a farbtastic instance inside the picker we've created.
-		var farb = $.farbtastic(picker, function(color) {
-			handler(color);
-		}).setColor($("#"+id+" input").val());
-
-		// When the "reset" link is clicked, reset the color.
-		$("#"+id+" .reset").click(function(e) {
-			e.preventDefault();
-			handler("");
-		}).toggle(!!$("#"+id+" input").val());
-
-	}
-
-	// Turn the "header color" field into a color picker.
-	colorPicker("headerColor", function(color, picker) {
-
-		// If no color is selected, use the default one.
-		color = color ? color : "#364159";
-
-		// Change the header's background color.
-		$("#hdr").css("backgroundColor", color);
-
-		// Unpack this color and convert it to HSL. If the lightness is > 0.5, set the "lightHdr" class on the body.
-		var rgb = $.farbtastic(picker).unpack(color);
-		var hsl = $.farbtastic(picker).RGBToHSL(rgb);
-		$("body").toggleClass("lightHdr", hsl[2] > 0.5);
-
-	});
-
-	// Turn the "body color" field into a color picker.
-	colorPicker("bodyColor", function(color, picker) {
-
-		// If no color is selected, use the default one.
-		color = color ? color : "#D6D8E6";
-
-		// Change the body's background color.
-		$("body").attr("style", "background-color:"+color+" !important");
-
-		// Unpack this color and convert it to HSL. If the lightness is < 0.5, set the "darkBody" class on the body.
-		var rgb = $.farbtastic(picker).unpack(color);
-		var hsl = $.farbtastic(picker).RGBToHSL(rgb);
-		$("body").toggleClass("darkBody", hsl[2] < 0.5);
-
-		// Slightly darken the body color and set it as the border color for the body content area.
-		hsl[2] = Math.max(0, hsl[2] - 0.1);
-		hsl[1] = Math.min(hsl[1], 0.5);
-		var b = $.farbtastic(picker).pack($.farbtastic(picker).HSLToRGB(hsl));
-		$("#body-content").css("borderColor", b);
-
-	});
-
-	// Add handlers to the background image checkbox.
-	$("#bodyImage input[name=bodyImage]").change(function(e) {
-		$("#bodyImage .indent").toggle($(this).prop("checked"));
-		if (!$(this).prop("checked")) {
-			$("body").css("backgroundImage", "none");
-		}
-	}).change();
+	// Turn the "primary color" field into a color picker.
+	colorPicker("primaryColor");
 
 });
 </script>
