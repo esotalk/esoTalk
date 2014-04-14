4c477676142eb70106f2caaccb33b95bc6a3d95e
Toby Zerner <toby.zerner@gmail.com>
2014-04-14 22:41:01 +0930

    add the answer icon as the last control instead of the first

--- addons/plugins/Answers/plugin.php
+++ addons/plugins/Answers/plugin.php
@@ -45,7 +45,7 @@ class ETPlugin_Answers extends ETPlugin {
 		if ($post["deleteMemberId"]) return;
 
 		if ($conversation["startMemberId"] == ET::$session->userId or $conversation["canModerate"]) {
-			addToArray($formatted["controls"], "<a href='".URL("conversation/answer/".$post["postId"]."?token=".ET::$session->token)."' title='".T("This post answered my question")."' class='control-answer'><i class='icon-ok-sign'></i></a>", 0);
+			addToArray($formatted["controls"], "<a href='".URL("conversation/answer/".$post["postId"]."?token=".ET::$session->token)."' title='".T("This post answered my question")."' class='control-answer'><i class='icon-ok-sign'></i></a>");
 		}
 
 		// If this post is the answer...
