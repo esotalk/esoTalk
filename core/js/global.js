f5e24e06cfbb07a7ae0eee6a6aaf6f098e300842
Toby Zerner <toby.zerner@gmail.com>
2014-04-14 22:49:32 +0930

    add colorPicker function to global

--- core/js/global.js
+++ core/js/global.js
@@ -1028,3 +1028,52 @@ updateTitle: function(number) {
 $(function() {
 	ETNotifications.init();
 });
+
+
+//***** COLOR PICKER
+
+// Turn a normal text input into a color picker, and run a callback when the color is changed.
+function colorPicker(id, callback) {
+
+	// Create the color picker container.
+	var picker = $("<div id='"+id+"-colorPicker'></div>").appendTo("body").addClass("popup").hide();
+
+	// When the input is focussed upon, show the color picker.
+	$("#"+id+" input").focus(function() {
+		picker.css({position: "absolute", top: $(this).offset().top - picker.outerHeight(), left: $(this).offset().left}).show();
+	})
+
+	// When focus is lost, hide the color picker.
+	.blur(function() {
+		picker.hide();
+	})
+
+	// When a value is typed in the field, update the color.
+	.keyup(function() {
+		farb.setColor($(this).val());
+	})
+
+	// Add a color swatch before the input.
+	.before("<span class='colorSwatch'></span>");
+
+	// Create a handler function for when the color is changed to update the input and swatch, and call
+	// the custom callback function.
+	var handler = function(color) {
+		if (typeof callback == "function") callback(color, picker);
+		$("#"+id+" input").val(color.toUpperCase());
+		$("#"+id+" .colorSwatch").css("backgroundColor", color);
+		$("#"+id+" .reset").toggle(!!color);
+	}
+
+	// Set up a farbtastic instance inside the picker we've created.
+	var farb = $.farbtastic(picker, function(color) {
+		handler(color);
+	}).setColor($("#"+id+" input").val());
+
+	// When the "reset" link is clicked, reset the color.
+	$("#"+id+" .reset").click(function(e) {
+		e.preventDefault();
+		handler("");
+	}).toggle(!!$("#"+id+" input").val());
+
+}
