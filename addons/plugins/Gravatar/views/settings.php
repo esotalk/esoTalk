be8d86ad28756f5aa3fe235a2e65aee0abf01be0
Tristan van Bokkem <tristanvanbokkem@gmail.com>
2014-04-18 15:08:18 +0200

    Fixed 404 on avatar.png

--- addons/plugins/Gravatar/views/settings.php
+++ addons/plugins/Gravatar/views/settings.php
@@ -32,12 +32,12 @@ $form = $data["gravatarSettingsForm"];
 <li>
 <label>Default imageset</label>
 <div class='checkboxGroup' id='gravatarDefaults'>
-	<label class='radio'><?php echo $form->radio("default", ""); ?> <img src='<?php echo getResource("core/skin/avatar.png"); ?>' class='avatar'> esoTalk default</label>
-	<label class='radio'><?php echo $form->radio("default", "mm"); ?> <img src='http://www.gravatar.com/avatar/0?d=mm' class='avatar'> Mystery Man</label>
-	<label class='radio'><?php echo $form->radio("default", "identicon"); ?> <img src='http://www.gravatar.com/avatar/0?d=identicon' class='avatar'> Identicon</label>
-	<label class='radio'><?php echo $form->radio("default", "monsterid"); ?> <img src='http://www.gravatar.com/avatar/0?d=monsterid' class='avatar'> MonsterID</label>
-	<label class='radio'><?php echo $form->radio("default", "wavatar"); ?> <img src='http://www.gravatar.com/avatar/0?d=wavatar' class='avatar'> Wavatar</label>
-	<label class='radio'><?php echo $form->radio("default", "retro"); ?> <img src='http://www.gravatar.com/avatar/0?d=retro' class='avatar'> Retro</label>
+	<label class='radio'><?php echo $form->radio("default", "blank"); ?> <img src='http://www.gravatar.com/avatar/0?d=blank&f=y' class='avatar'> esoTalk default</label>
+	<label class='radio'><?php echo $form->radio("default", "mm"); ?> <img src='http://www.gravatar.com/avatar/0?d=mm&f=y' class='avatar'> Mystery Man</label>
+	<label class='radio'><?php echo $form->radio("default", "identicon"); ?> <img src='http://www.gravatar.com/avatar/0?d=identicon&f=y' class='avatar'> Identicon</label>
+	<label class='radio'><?php echo $form->radio("default", "monsterid"); ?> <img src='http://www.gravatar.com/avatar/0?d=monsterid&f=y' class='avatar'> MonsterID</label>
+	<label class='radio'><?php echo $form->radio("default", "wavatar"); ?> <img src='http://www.gravatar.com/avatar/0?d=wavatar&f=y' class='avatar'> Wavatar</label>
+	<label class='radio'><?php echo $form->radio("default", "retro"); ?> <img src='http://www.gravatar.com/avatar/0?d=retro&f=y' class='avatar'> Retro</label>
 </div>
 </li>
 
