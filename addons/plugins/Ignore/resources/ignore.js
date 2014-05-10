74061b780b968f4faafbd28afd2b62a032a49969
Toby Zerner <toby.zerner@gmail.com>
2014-05-10 13:27:12 +0930

    extract all plugins except for BBCode, Debug, Emoticons, and SMTP into separate repositories

--- addons/plugins/Ignore/resources/ignore.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('#conversationPosts').on('click', '.ignoredInfo a.ignoredShow', function(e) {
-		e.preventDefault();
-		$(this).parents('.post').removeClass('ignored');
-		$(this).parent().remove();
-	});
-});
