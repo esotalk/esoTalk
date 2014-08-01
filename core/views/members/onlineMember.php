e2d2cfaf070b437721ea7e755ed2bef819da3956
Toby Zerner <toby.zerner@gmail.com>
2014-08-01 14:56:30 +0930

    Remove lcfirst (more trouble than it's worth for UTF-8 support). Fixes #292

--- core/views/members/onlineMember.php
+++ core/views/members/onlineMember.php
@@ -16,7 +16,7 @@ $member = $data["member"];
 <?php echo avatar($member, "thumb"), " ", memberLink($member["memberId"], $member["username"]), " "; ?>
 <?php
 $action = ET::memberModel()->getLastActionInfo($member["lastActionTime"], $member["lastActionDetail"]);
-if ($action[0]) printf(T("is %s"), (!empty($action[1]) ? "<a href='{$action[1]}'>" : "").lcfirst(sanitizeHTML($action[0])).(!empty($action[1]) ? "</a>" : ""));
+if ($action[0]) printf(T("is %s"), (!empty($action[1]) ? "<a href='{$action[1]}'>" : "").sanitizeHTML($action[0]).(!empty($action[1]) ? "</a>" : ""));
 ?>
 </span>
 </li>
